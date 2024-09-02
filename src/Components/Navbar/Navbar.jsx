e27ece72b70cf57c28348f0233290a5d96e694b1
NicholasJardine <nicholasjardine0@gmail.com>
2024-09-02 10:44:29 +0200

    feature: added initial stages of Hero section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li>About</li>
         <li>Courses</li>
         <li>Testimonials</li>
-        <li>Contact us</li>
+        <li><button className='btn'>Contact us</button></li>
       </ul>
     </nav>
   )
