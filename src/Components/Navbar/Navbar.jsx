8f10c449f6f9a92a2a1290c0f5e8c711ff5f4f7d
NicholasJardine <nicholasjardine0@gmail.com>
2024-09-04 10:19:51 +0200

    feature: added mediqueries for responsivity

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
+
 const Navbar = () => {
     const [sticky, setSticky] =useState(false);
 
@@ -9,17 +11,23 @@ const Navbar = () => {
         window.addEventListener('scroll',()=>{
             window.scrollY >50 ? setSticky(true) : setSticky(false)
         })
-    },[])
+    },[]);
+
+    const[mobileMenu, setMobileMenu] = useState(false)
+    const toggleMenu = ()=>{
+      mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+    }
   return (
     <nav className={`container ${sticky? 'dark-nav': ''}`}>
       <img src={logo} alt=""  className='logo'/>
-      <ul>
+      <ul className={mobileMenu? '': 'hide-menu'}>
         <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li> <Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
         <li> <Link to='bootcamps' smooth={true} offset={-260} duration={500}>Courses</Link></li>
         <li> <Link to='reviews' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
         <li><Link className='btn' to='contact' smooth={true} offset={-260} duration={500}>Contact us</Link></li>
       </ul>
+      <img src={menu_icon} alt="" className='menu' onClick={toggleMenu}/>
     </nav>
   )
 }
